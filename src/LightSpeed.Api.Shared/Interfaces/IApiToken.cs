a8c9809f000a8012cf795f575f38ce25aa9814d4
Brendan Gooden <brendan@catamac.com.au>
2023-06-30 07:14:47 +0930

    chore: remove obsolete files.

--- src/LightSpeed.Api.Shared/Interfaces/IApiToken.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LightSpeed.Api.Shared.Interfaces;
-
-public interface IApiToken
-{
-    /// <summary>
-    /// Personal Access Token (PAT) used to communicate with the Vend API's
-    /// </summary>
-    string ApiToken { get; }
-}
