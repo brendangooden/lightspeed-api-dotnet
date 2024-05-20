3a83b8c77a47f12c186cbef5e0f57af5a1717659
Brendan Gooden <30429990+brendangooden@users.noreply.github.com>
2024-05-21 06:54:15 +0930

    chore: add documentation

--- src/LightSpeed.Api/Configuration/TokenApiConfiguration.cs
+++ src/LightSpeed.Api/Configuration/TokenApiConfiguration.cs
@@ -8,8 +8,19 @@ namespace LightSpeed.Api.Configuration;
 /// </summary>
 public class TokenApiConfiguration : ILightSpeedApiConfiguration
 {
+    /// <summary>
+    /// The personal token used to authenticate with the LightSpeed API.
+    /// </summary>
     public required string ApiToken { get; init; }
+    /// <summary>
+    /// The domain of the store to connect to.
+    /// </summary>
     public required string StoreDomain { get; init; }
+    /// <summary>
+    /// Create a new instance of the <see cref="TokenApiConfiguration"/> class.
+    /// </summary>
+    /// <param name="storeDomain"></param>
+    /// <param name="apiToken"></param>
     [SetsRequiredMembers]
     public TokenApiConfiguration(string storeDomain, string apiToken)
     {
